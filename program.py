8d9116a8559127eabbcb0671943d281e14aaac49
Pedram Asshabi <pedramas@live.com>
2018-12-02 21:34:02 +0330

    Fixed data import.

--- program.py
+++ program.py
@@ -11,6 +11,9 @@ dtype_dict = {'bathrooms': float, 'waterfront': int, 'sqft_above': int, 'sqft_li
               'sqft_lot15': float, 'sqft_living': float, 'floors': float, 'condition': int, 'lat': float, 'date': str,
               'sqft_basement': int, 'yr_built': int, 'id': str, 'sqft_lot': int, 'view': int}
 
-train = pd.read_csv('wk3_kc_house_train_data.csv', dtype=dtype_dict)
-train_valid_shuffled = pd.read_csv('wk3_kc_house_train_valid_shuffled.csv', dtype=dtype_dict)
-test = pd.read_csv('wk3_kc_house_test_data.csv', dtype=dtype_dict)
+data = pd.read_csv('kc_house_data.csv', dtype=dtype_dict)
+train = pd.read_csv('kc_house_train_data.csv', dtype=dtype_dict)
+test = pd.read_csv('kc_house_test_data.csv', dtype=dtype_dict)
+
+
+
