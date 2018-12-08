d984c4afb31909176c27c160e98eff7a124e1bcb
Pedram Asshabi <pedramas@live.com>
2018-12-09 00:29:42 +0330

    Revert "All bugs and errors fixed, All function tested separately. assignment week 4 - 2 has finished."
    This reverts commit 1c88e5d

--- learnt/regression/predict_outcome.py
+++ learnt/regression/predict_outcome.py
@@ -4,11 +4,16 @@ import numpy as np
 def predict_outcome(feature_matrix, weights):
     """
 
-    :param feature_matrix: N×D matrix
+    :param feature_matrix: N×D
     :param weights: Estimated weights by gradient decent algorithm. its a vector 1×D
-    :return: predicted output base on the given weights. It is a N×1 vector
+    :return: (ndarray) predicted output base on the given weights. It is a 1×N vector
     """
+    # make sure it's a numpy array:
+    if type(weights) == list:
+        weights = np.array(weights)
+    if type(feature_matrix) == list:
+        feature_matrix = np.array(feature_matrix)
+
     weights = weights.reshape(-1, 1)
     predictions = np.dot(feature_matrix, weights)
-    return predictions
-
+    return predictions.reshape(1, -1)  # reshape vector to 1×N (default shape).
